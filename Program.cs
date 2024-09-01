929a5d2d1822186d285fdd28c71ebe097c69ad38
Andy <zhumeilun0103@gmail.com>
2024-09-01 13:08:56 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.Write("Please input temperature in degrees Fahrenheit: ");
+string input = Console.ReadLine();
+
+int degF = Convert.ToInt32(input);
+
+double degC = 5 * (degF - 32.0) / 9.0;
+
+Console.WriteLine("{0} degrees Fahrenheit = {1} degrees Celsius",input,Math.Round(degC,1));
+
